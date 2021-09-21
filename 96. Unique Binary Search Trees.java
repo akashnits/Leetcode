9801a3f37da72e6a3450e05dabf2728c88fb2b2d
Akash <akash.nits@outlook.com>
2021-09-21 14:02:47 +0530

    Update 96. Unique Binary Search Trees.java

--- 96. Unique Binary Search Trees.java
+++ 96. Unique Binary Search Trees.java
@@ -1,23 +1,40 @@
 class Solution {
-    public int numTrees(int n) {
-        //formula for this: G(n) = F(1, n) + F(2 ,n) + F(3, n)... + F(n, n)
-        // where G(n)= summation of all F(i, n) till n
-        //F(i,n) is the number of unique bst rooted at i
-        
-        //rewriting abobe equation
-        // G(n)= G(0) * G(n-1) + G(1) * G(n-2) + G(2) * G(n-3)...  + G(n-1) * G(0)
-        
-        int[] dp= new int[n+1];
-        dp[0]= 1;
-        dp[1]= 1;
-        
-         for(int i=2; i<=n; i++) {
-            for(int j=1; j<=i; j++) {
-            // dp[i] is number of bst that can be constructed with sequence 1,2,3...i
-            // summing bst rooted at 1, 2, 3.. i will give dp[i]    
-            dp[i] += dp[j-1] * dp[i-j];
-            }
+    //recursive approach
+    
+     public int numTrees(int n) {
+        int sum=0;
+        //base condition
+        if( n <= 1){
+            return 1;
         }
+        
+        for(int i=1; i <=n ; i++){
+            sum += numTrees(i-1) * numTrees(n-i);
+        }
+        
+        return sum;
+    }
+    
+    // dp approach
+    //here dp array contains the number of unique BST's which has exactly n nodes of unique values from 1 to n.
+    
+    public int numTrees(int n) {
+        return solve(n , new int[n+1]);
+    }
+    
+    int solve(int n, int[] dp){
+        //base condition
+        if( n <= 1){
+            dp[n] =1;
+            return 1;
+        }
+        
+        for(int i=1; i <=n ; i++){
+            dp[i-1] = dp[i-1] == 0 ? solve(i-1, dp): dp[i-1];
+            dp[n-i] = dp[n-i] == 0 ? solve(n-i, dp): dp[n-i];
+            dp[n] += dp[i-1] * dp[n-i];
+        }
+        
         return dp[n];
     }
 }
