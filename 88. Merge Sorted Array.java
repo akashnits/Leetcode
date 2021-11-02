5f346e6c80b51def2b65995a310c056fd3a50143
Akash <akash.nits@outlook.com>
2021-11-02 14:11:32 +0530

    Create 88. Merge Sorted Array.java

---
+++ 88. Merge Sorted Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Idea: 1. start filling from back to avoid displacment
+    // 2. Think about filling element at position k initialized to  m+n-1
+    // we can fill position k from nums1[i] or nums2[j] 
+    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m+n-1;
+        int i= m-1;
+        int j= n-1;
+        
+        while(i >=0 && j >= 0){
+            if(nums1[i] <= nums2[j]){
+                nums1[k--]= nums2[j--];
+            }else{
+                nums1[k--]= nums1[i--];
+            }
+        }
+        
+        while(j >= 0){
+            nums1[k--]= nums2[j--];
+        }
+        
+    }
+}
