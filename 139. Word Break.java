93bb17c4aee14b6dae9fd1593aba41aefe6c840c
Akash <akash.nits@outlook.com>
2022-07-10 03:32:25 +0530

    Update 139. Word Break.java

--- 139. Word Break.java
+++ 139. Word Break.java
@@ -32,3 +32,48 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+    // state variable -> start varies from 0 to n
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Boolean[] dp = new Boolean[n];
+        return solve(s, wordDict, 0, dp);
+    }
+    
+    boolean solve(String s, List<String> wordDict, int start, Boolean[] dp){
+        
+        // base condition
+        if(start == s.length()){
+            return true;
+        }
+        
+        if(dp[start] != null){
+            return dp[start];
+        }
+        
+        // choice diagram
+        for(String word: wordDict){
+            if(canPick(word, s, start)){
+                if(solve(s, wordDict, start+ word.length(), dp)){
+                    dp[start] = true;
+                    return true;
+                }
+            }
+        }
+        dp[start] = false;
+        return dp[start];
+    }
+    
+    
+    boolean canPick(String word, String s, int start){
+        
+        int end = start + word.length();
+        if(end > s.length()){
+            return false;
+        }
+        String toCompare= s.substring(start, end);
+        return toCompare.equals(word);
+    }
+}
