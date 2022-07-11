5d8a0b40d299cf468a95bcd5e31d579fe8ed2938
Akash <akash.nits@outlook.com>
2022-07-12 00:56:55 +0530

    Update 746. Min Cost Climbing Stairs.java

--- 746. Min Cost Climbing Stairs.java
+++ 746. Min Cost Climbing Stairs.java
@@ -1,16 +1,26 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        // need to reach top floor i.e. dp[n+1]
-        int n = cost.length -1;
-        int dp[] = new int[n+2];
-        // dp[i] -> min cost to reach ith step
-        dp[0] = 0;
-        dp[1] = 0;
+        int n = cost.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
         
-        for(int i =2; i <= cost.length; i++){
-            dp[i] = Math.min(dp[i-2] + cost[i-2], dp[i-1]+ cost[i-1]);
+        return Math.min(solve(cost, 0, dp), solve(cost, 1, dp));
+    }
+    
+    
+    int solve(int[] cost, int i, int[] dp) {
+        // base condition
+        if(i >= cost.length){
+            return 0;
         }
         
-        return dp[n+1];
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        
+        // choices - can climb 1 or 2 after paying up
+        
+        dp[i] =  cost[i]  + Math.min(solve(cost, i+1, dp), solve(cost, i+2, dp));
+        return dp[i];
     }
 }
