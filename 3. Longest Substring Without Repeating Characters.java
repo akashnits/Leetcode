98d993261c5c361e68a5826306900c8cb7fdc659
Akash <akash.nits@outlook.com>
2022-07-12 02:01:14 +0530

    Update 3. Longest Substring Without Repeating Characters.java

--- 3. Longest Substring Without Repeating Characters.java
+++ 3. Longest Substring Without Repeating Characters.java
@@ -5,26 +5,29 @@ class Solution {
         int n = s.length();
         Set<Character> windowSet = new HashSet();
         int maxLength =0;
-        int duplicate = 0;
+        int repeat = 0;
         while(hi < n){
             // expand here
-            char c = s.charAt(hi);
+            char charAtHi = s.charAt(hi);
             
-            if(windowSet.contains(c)){
-                duplicate = 1;
-                // we need shrink until we drop c from left
-                while( lo<=hi && duplicate == 1){
-                    if(s.charAt(lo) == c){
-                        // we drop it
-                        duplicate=0;
-                    }
-                    windowSet.remove(s.charAt(lo));
-                    lo++;
-                }
+            if(windowSet.contains(charAtHi)){
+                repeat++;
+            }else{
+                windowSet.add(charAtHi);
             }
-            windowSet.add(c); // adds to set if not already present
-            maxLength = Math.max(maxLength, hi-lo+1);
+            
+           // shrink here
+            while(repeat == 1 && lo <= hi){ // make this window undesired
+                char charAtLo = s.charAt(lo);
+                if(charAtLo == charAtHi){
+                    --repeat;
+                }else{
+                    windowSet.remove(charAtLo);
+                }
+                lo++;
+            }
 
+            maxLength = Math.max(maxLength, hi-lo+1);
             hi++;
         }
         return maxLength;
