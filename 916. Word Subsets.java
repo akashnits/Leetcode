db013cd18babf75e301da8a3026c86efddce7b37
Akash <akash.nits@outlook.com>
2022-08-01 00:58:55 +0530

    Create 916. Word Subsets.java

---
+++ 916. Word Subsets.java
@@ -0,0 +1,61 @@
+class Solution {
+    
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        
+        List<String> result = new ArrayList();
+        for(String w1: words1){
+            result.add(w1);
+        }
+        
+        int[] maxFreq = new int[26];
+        
+       for(String w2: words2){
+           int[] freq = new int[26];
+           for(char c: w2.toCharArray()){
+               freq[c-'a']++;
+               maxFreq[c-'a'] = Math.max(maxFreq[c-'a'], freq[c-'a']);
+           }
+       }
+    
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i < 26; i++){
+            if(maxFreq[i] > 0){
+                char c = (char) ('a' + i); 
+                String s= Character.toString(c);
+                while(maxFreq[i]-- > 0){
+                    sb.append(s);
+                }
+            }
+        }
+        
+        ListIterator<String> itr = result.listIterator();
+        while(itr.hasNext()){
+            String next = itr.next();
+            if(!isSubset(next, sb.toString())){
+                itr.remove();
+            }
+        }
+        
+        
+        return result;
+    }
+    
+    
+    boolean isSubset(String a, String b){
+        // check if b is subset of a i.e.  if every letter in b occurs in a including multiplicity.
+        int[] freq = new int[26];
+        
+        for(char c1: a.toCharArray()){
+            freq[c1-'a']++;
+        }
+        
+        for(char c2: b.toCharArray()){
+            if(freq[c2-'a'] == 0){
+                return false;
+            }else{
+                freq[c2-'a']--;
+            }
+        }
+        return true;
+    }
+}
