6e0d006551c592447674bc6582ec1fcce3a121c4
Akash <akash.nits@outlook.com>
2022-08-10 18:52:20 +0530

    Create 1220. Count Vowels Permutation.java

---
+++ 1220. Count Vowels Permutation.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int countVowelPermutation(int n) {
+        // To avoid the large output value
+        int MOD = (int)(1e9 + 7);
+ 
+        // dp[i][char] represents number of ways to reach char when path length is i
+        // Initialize 2D dp array
+        long[][] dp = new long[n + 1][5];
+ 
+        // Initialize dp[1][i] as 1 since
+        // string of length 1 will consist
+        // of only one vowel in the string
+        for (int i = 0; i < 5; i++) {
+            dp[1][i] = 1; // path length to self is one
+        }
+ 
+        // Directed graph using the
+        // adjacency matrix
+        int[][] relation = new int[][] {
+            { 1 }, { 0, 2 },
+            { 0, 1, 3, 4 },
+            { 2, 4 }, { 0 }
+        };
+ 
+        // we need to find all N length paths ending at all chars (a,e,i,o,u)
+        
+        // Iterate over the range [2, N], path length
+        for (int i = 2; i <= n; i++) {
+ 
+            // for each vertex
+            for (int u = 0; u < 5; u++) {
+                dp[i][u] = 0; // no way to reach self with path length > 1
+ 
+                // Traversing the neighbors
+                for (int v : relation[u]) {
+ 
+                    // we have path fron u -> v
+                    // so, i length path to reach u is sum of  i-1 length path to all it's neightbors
+                    dp[i][u] += dp[i-1][v] % MOD;
+                }
+            }
+        }
+ 
+        // summing n-length path to reach all chars gives the ans
+        long ans = 0;
+ 
+        for (int i = 0; i < 5; i++) {
+            ans = (ans + dp[n][i]) % MOD;
+        }
+ 
+        // Return count of permutations
+        return (int)ans;
+    }
+}
