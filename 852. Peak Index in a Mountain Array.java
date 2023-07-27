13bf5f2a0daa5063bcc402923f574cdbb949c380
Akash <akash.nits@outlook.com>
2023-07-27 13:28:34 +0530

    Update 852. Peak Index in a Mountain Array.java

--- 852. Peak Index in a Mountain Array.java
+++ 852. Peak Index in a Mountain Array.java
@@ -1,30 +1,40 @@
 class Solution {
+    // need to find localMax i.e. arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]
+    // decision space: we move to find localmax, so we go in the direction where arr[mid+1] or arr[mid-1] > arr[mid] 
     public int peakIndexInMountainArray(int[] arr) {
-        return binarySearch(arr, 0, arr.length-1, arr.length);
+        return binarySearchLocalMax(arr, 0, arr.length-1);
     }
-    
-    private int binarySearch(int[] arr, int l, int r, int n){
-        if(l > r){
+
+    int binarySearchLocalMax(int[] arr, int start, int end ){
+        if(start > end){
             return -1;
         }
-        
-        int mid = l + (r-l)/2;
-        
-        
-        int prev = mid == 0? n-1: mid-1;
-        int next = mid == n-1? 0: mid+1;
-       
-            if(arr[mid] > arr[prev] && arr[mid] > arr[next]){
-                return mid;
-            }else if(arr[next] > arr[mid]){
-                // move right
-                return binarySearch(arr, mid+1, r, n);
-            }else{
-                // move left
-                return binarySearch(arr, l, mid-1, n);
-            }
-            
-        
-        
+
+        int mid = start + (end-start)/2;
+
+        // check if arr[mid] is localMax
+
+        if(mid == arr.length-1 && arr[mid] > arr[mid]-1){ // mid is at the last index
+            return mid;
+        }
+
+        if(mid == 0 && arr[mid] > arr[mid]+1){ // mid at the start
+            return mid;
+        }
+
+        if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
+            return mid;
+        }
+
+        // we keep searching in decision space
+        if(arr[mid+1] > arr[mid]){
+            // move right
+            return binarySearchLocalMax(arr, mid+1, end);
+        } else{
+            //move left
+            return binarySearchLocalMax(arr, start, mid-1);
+        }
+
     }
+
 }
