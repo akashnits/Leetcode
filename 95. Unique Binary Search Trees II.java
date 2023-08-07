cc55cb868e0f40857359f2b98c5e4b7a2b206730
Akash <akash.nits@outlook.com>
2023-08-07 20:06:19 +0530

    Update 95. Unique Binary Search Trees II.java

--- 95. Unique Binary Search Trees II.java
+++ 95. Unique Binary Search Trees II.java
@@ -14,39 +14,49 @@
  * }
  */
 class Solution {
-    List<TreeNode> result = new ArrayList();
     public List<TreeNode> generateTrees(int n) {
-        return constructBSTs(1, n);
+        return constructBSTs(0, n-1);
     }
-    
-    
-    public List<TreeNode> constructBSTs(int start, int end){
-        List<TreeNode> list = new ArrayList<TreeNode>();
 
+
+    List<TreeNode> constructBSTs(int start, int end){
+        List<TreeNode> nodeList =  new ArrayList();
+        // base condition
         if(start > end){
-            list.add(null);
-            return list;
+            nodeList.add(null);
+            return nodeList;
         }
-        
-         if(start == end){
-            list.add(new TreeNode(start));
-            return list;
+
+        // only one node
+        if(start == end){
+            TreeNode node = new TreeNode(start+1);
+            nodeList.add(node);
+            return nodeList;
         }
-        
-        List<TreeNode> left, right;
-        for(int i=start; i<=end ; i++){
-            //each i as root val
-            left = constructBSTs(start, i-1);
-            right = constructBSTs(i+1, end);
-            for(TreeNode l : left){
-                for(TreeNode r: right){
-                    TreeNode root = new TreeNode(i);
-                    root.left = l;
-                    root.right = r;
-                    list.add(root);
+
+        List<TreeNode> res = new ArrayList();
+
+        // choices- make each ele root 
+        for(int i= start; i <= end; i++){
+            // we can construct left BSTs with nodes which is left of i -> List<TreeNode>
+            // we can contruct right BSTs with nodes which is right of i -> List<TreeNode>
+            List<TreeNode> leftBSTs = constructBSTs(start, i-1);
+            List<TreeNode> rightBSTs = constructBSTs(i+1, end);
+
+            // say left BSTs = B1, B2, B3
+            // right BSTs = B4, B5
+            // we can have a tree as [B1, root, B4], [B2, root, B4], so on....
+            for(TreeNode leftNode: leftBSTs){
+                for(TreeNode rightNode: rightBSTs){
+                    // we permute all leftBST with rightBSTs
+                    TreeNode root = new TreeNode(i+1);
+                    root.left = leftNode;
+                    root.right = rightNode;
+                    res.add(root);
                 }
             }
         }
-        return list;
+
+        return res;
     }
 }
