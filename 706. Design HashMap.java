03a7b058d1049463c67b0328a6b1d71ddc017192
Akash <akash.nits@outlook.com>
2023-10-04 12:06:55 +0530

    Update 706. Design HashMap.java

--- 706. Design HashMap.java
+++ 706. Design HashMap.java
@@ -1,71 +1,51 @@
-class MyHashMap {
-    // Use ListNode, bucket and chaining
+class MyHashMap{
+        final ListNode[] nodes = new ListNode[1000];
 
-	ListNode[] nodes = new ListNode[10000];
+        public void put(int key, int value){
+            int i = idx(key);
+            if(nodes[i] == null)
+                nodes[i] = new ListNode(-1, -1);
+            ListNode prev = find(nodes[i], key);
+            if(prev.next == null)
+                prev.next = new ListNode(key, value);
+            else prev.next.val = value;
+        }
 
-	public int get(int key)
-	{
-		int index = getIndex(key);
-		ListNode prev = findElement(index, key);
-		return prev.next == null ? -1 : prev.next.val;
-	}
-	
-	public void put(int key, int value)
-	{
-		int index = getIndex(key);
-		ListNode prev = findElement(index, key);
-		
-		if (prev.next == null)
-			prev.next = new ListNode(key, value);
-		else 
-			prev.next.val = value;
-	}
+        public int get(int key){
+            int i = idx(key);
+            if(nodes[i] == null)
+                return -1;
+            ListNode node = find(nodes[i], key);
+            return node.next == null ? -1 : node.next.val;
+        }
 
-	public void remove(int key)
-	{
-		int index = getIndex(key);
-        ListNode prev = findElement(index, key);
-			
-        if(prev.next != null)
-		    prev.next = prev.next.next;
-	}
+        public void remove(int key){
+            int i = idx(key);
+            if(nodes[i] != null){
+                ListNode prev = find(nodes[i], key);
+                if(prev.next != null)
+                    prev.next = prev.next.next;
+            }
+        }
 
-	private int getIndex(int key)
-	{	
-		return Integer.hashCode(key) % nodes.length;
-	}
+        int idx(int key){return key % nodes.length;}
 
-	private ListNode findElement(int index, int key)
-	{
-		if(nodes[index] == null)
-			return nodes[index] = new ListNode(-1, -1);
-        
-        ListNode prev = nodes[index];
-		
-		while(prev.next != null && prev.next.key != key)
-		{
-			prev = prev.next;
-		}
-		return prev;
-	}
+        ListNode find(ListNode bucket, int key){
+            ListNode node = bucket, prev = null;
+            while(node != null && node.key != key){
+                prev = node;
+                node = node.next;
+            }
+            return prev;
+        }
 
-	private static class ListNode
-	{
-		int key, val;
-		ListNode next;
+        class ListNode{
+            int key, val;
+            ListNode next;
 
-		ListNode(int key, int val)
-		{
-			this.key = key;
-			this.val = val;
-		}
-	}
-}
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
+            ListNode(int key, int val){
+                this.key = key;
+                this.val = val;
+            }
+        }
+    }
