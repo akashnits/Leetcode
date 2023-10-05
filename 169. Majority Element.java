aa94d031e623d15eaed67176af76a11bf7bfab40
Akash <akash.nits@outlook.com>
2023-10-05 12:38:58 +0530

    Update 169. Majority Element.java

--- 169. Majority Element.java
+++ 169. Majority Element.java
@@ -1,13 +1,20 @@
 class Solution {
+    // Approach: opponents reduce vote by 1
     public int majorityElement(int[] nums) {
-        int count =0;
-        int candidate=-1;
-        for(int num: nums){
-            if(count == 0){
-                // pick a new candidate
-                candidate = num;
+        int candidate = nums[0];
+        int vote =0;
+
+        for(int i=0; i < nums.length; i++){
+            // cases we have 
+            if(vote == 0){ //case1: candidate doesn't have a vote
+                // make nums[i] candidate
+                candidate = nums[i];
+                vote++;
+            }else if(candidate == nums[i]){ // case2: nums[i] is the candidate element
+                vote++;
+            }else { //case2: nums[i] is NOT the candidate element
+                vote--;
             }
-            count = (candidate == num ? ++count: --count);
         }
         return candidate;
     }
