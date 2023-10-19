beb029aa082414541bc7a19e07e657b8c077a102
Akash <akash.nits@outlook.com>
2023-10-19 14:13:47 +0530

    Update 210. Course Schedule II.java

--- 210. Course Schedule II.java
+++ 210. Course Schedule II.java
@@ -1,77 +1,49 @@
 class Solution {
-    Stack<Integer> stack;
-    int visited[];
-    ArrayList<ArrayList<Integer>> graph;
+    // use topological sort using Kahn's algo
+    int[] res;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
-        int n = numCourses;
-         graph = new ArrayList();
-         visited= new int[n];
-         // create directed graph from given pre-requisities
-         for(int i=0; i < n; i++){
-             graph.add(new ArrayList());
-         }
+        res = new int[numCourses];
+        // create adjacency list
+        List<List<Integer>> graph = new ArrayList();
+        int V = numCourses;
 
-         for(int[] row: prerequisites){
-             int u = row[0];
-             int v= row[1];
-             // add edge - directed graph
-             graph.get(v).add(u);
-         }
-        
-        // check of cycle 
-         for(int k=0; k < n; k++){
-             if(visited[k] == 0 && isDeadLocked(k)){
-                 return new int[0];
-             }
-         }
-        visited = new int[n];
-        // find topological order
-        stack = new Stack();
-         for(int k=0; k < n; k++){
-             if(visited[k] == 0){
-                topologicalSort(k);
-             }
-         }
-        int[] result = new int[n];
-        int i=0;
-        while(!stack.isEmpty()){
-            result[i]= stack.pop();
-            i++;
+        for(int i=0; i < V; i++){
+            graph.add(new ArrayList());
         }
-        return result;
-    }
-    
-    // anytime we come across a vertex which is under process, we know that
-     // we have reached it from decendant, hence deadlock/loop
-     private boolean isDeadLocked(int v){
-         visited[v] = 1; // under process
-         for(int child: graph.get(v)){
-             if(visited[child] == 0){ // unvisited
-                 if(isDeadLocked(child)){
-                     return true;
-                 } 
-             }else{
-                 // it's visited and under process
-                 if(visited[child] == 1){
-                     return true;
-                 }
 
-             }
-         }
-         visited[v] = 2;
-         return false;
-     }
-    
-    
-    private void topologicalSort(int v){
-        
-        visited[v] = 1;
-        for(int child: graph.get(v)){
-            if(visited[child] == 0){
-                topologicalSort(child);
+        int[] indegree = new int[V];
+
+        // add edges and build indegree array
+        for(int[] edge: prerequisites){
+            graph.get(edge[1]).add(edge[0]);
+            indegree[edge[0]]++;
+        }
+
+        Queue<Integer> queue = new ArrayDeque();
+
+        // loop over indegree array and find vertices with indegree value == 0
+        for(int v=0; v < V; v++){
+            if(indegree[v] == 0){
+                queue.add(v);
             }
         }
-        stack.push(v);
+
+        // toposort
+        return toposort(queue, indegree, graph, V) ? res: new int[0];
+    }
+
+    boolean toposort(Queue<Integer> queue, int[] indegree, List<List<Integer>> graph, int V){
+        int nodesVisited = 0;
+        while(!queue.isEmpty()){
+            int polled = queue.poll();
+            res[nodesVisited++] = polled;
+            // iterate its neighbor and decrease indegree
+            for(int neighbor: graph.get(polled)){
+                if(--indegree[neighbor] == 0){
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return nodesVisited == V;
     }
 }
