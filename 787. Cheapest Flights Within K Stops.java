96d0c77779b391fb697c36b54e1a6cfc11f99737
Akash <akash.nits@outlook.com>
2024-02-26 13:33:46 +0530

    Update 787. Cheapest Flights Within K Stops.java

--- 787. Cheapest Flights Within K Stops.java
+++ 787. Cheapest Flights Within K Stops.java
@@ -1,37 +1,51 @@
+import java.util.*;
+
 class Solution {
-    
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-     
-        // We use two arrays for storing distances and keep swapping
-        // between them to save on the memory
-        long[][] distances = new long[2][n];
-        Arrays.fill(distances[0], Integer.MAX_VALUE);
-        Arrays.fill(distances[1], Integer.MAX_VALUE);
-        distances[0][src] = distances[1][src] = 0;
-        
-        // K + 1 iterations of Bellman Ford
-        for (int iterations = 0; iterations < K + 1; iterations++) {
-            
-            // Iterate over all the edges
-            for (int[] edge : flights) {
-                
-                int s = edge[0], d = edge[1], wUV = edge[2];
-                
-                // Current distance of node "s" from src
-                long dU = distances[1 - iterations&1][s];
-                
-                // Current distance of node "d" from src
-                // Note that this will port existing values as
-                // well from the "previous" array if they didn't already exist
-                long dV = distances[iterations&1][d];
-                
-                // Relax the edge if possible
-                if (dU + wUV < dV) {
-                    distances[iterations&1][d] = dU + wUV;
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] distance = new int[n];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+
+        for (int[] flight : flights) {
+            int u = flight[0];
+            int v = flight[1];
+            int cost = flight[2];
+
+            if (!adj.containsKey(u)) {
+                adj.put(u, new ArrayList<>());
+            }
+            adj.get(u).add(new int[]{v, cost});
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{src, 0});
+        distance[src] = 0;
+
+        int level = 0;
+
+        while (!queue.isEmpty() && level <= k) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                int u = current[0];
+                int costU = current[1];
+
+                List<int[]> neighbors = adj.getOrDefault(u, new ArrayList<>());
+                for (int[] neighbor : neighbors) {
+                    int v = neighbor[0];
+                    int costUV = neighbor[1];
+
+                    if (distance[v] > costU + costUV) {
+                        distance[v] = costU + costUV;
+                        queue.offer(new int[]{v, distance[v]});
+                    }
                 }
             }
+            level++;
         }
-        
-        return distances[K&1][dst] < Integer.MAX_VALUE ? (int)distances[K&1][dst] : -1;
+
+        return distance[dst] == Integer.MAX_VALUE ? -1 : distance[dst];
     }
 }
