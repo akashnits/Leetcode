8805b9dc9d125fdf4c0c60d60e9649c714024698
Akash <akash.nits@outlook.com>
2024-04-19 13:43:46 +0530

    Update 137. Single Number II.java

--- 137. Single Number II.java
+++ 137. Single Number II.java
@@ -1,15 +1,24 @@
 class Solution {
-    // one = ( one ^ num ) & ( 1's complement of two )
-    // two = ( two ^ num ) & ( 1's complement of one )
-    // detailed explanation: https://leetcode.com/problems/single-number-ii/solutions/43295/detailed-explanation-and-generalization-of-the-bitwise-operation-method-for-single-numbers/
-    
+    // approach: count set bits at each position ( 32 bits) and take %3 to see if it 
+    // occurs less than 3 i.e. just once
+    // we include this bit in the answer
     public int singleNumber(int[] nums) {
-        int ones = 0, twos= 0;
-        for(int i=0; i< nums.length; i++){
-            ones = (ones ^ nums[i]) & ~twos;
-            twos = (twos ^ nums[i]) & ~ones;
-        }
+        int ans = 0;
+        int n = nums.length;
+        int mask = 1;
 
-        return ones;
+        // loop over all bits
+        for(int i=0; i < 32; i++){
+            int count = 0; // count set bits at position i
+            for(int num: nums){
+                count += (num >> i) & 1; // rightShift the number by i bits and AND with 1 to find 0 or 1 i.e. bit is set or not
+            }
+
+            // check if count is multiple of 3, if not include in answer
+            if(count % 3 != 0){
+                ans += (1<<i);
+            }
+        }
+        return ans;
     }
 }
