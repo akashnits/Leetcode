5029745e6ff63f3487f8b6b86f55b725ab53ee22
Akash <akash.nits@outlook.com>
2024-08-29 16:07:03 +0530

    Create 264. Ugly Number II.java

---
+++ 264. Ugly Number II.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int nthUglyNumber(int n) {
+        // TreeSet to store potential ugly numbers
+        TreeSet<Long> uglyNumbersSet = new TreeSet<>(); 
+        uglyNumbersSet.add(1L); // Start with 1, the first ugly number
+        // TreeSet automatically sorts elements in ascending order and does not
+        // allow duplicate entries
+
+        Long currentUgly = 1L;
+        for (int i = 0; i < n; i++) {
+            currentUgly = uglyNumbersSet.pollFirst(); // Get the smallest number from the set and remove it
+
+            // Insert the next potential ugly numbers into the set
+            uglyNumbersSet.add(currentUgly * 2);
+            uglyNumbersSet.add(currentUgly * 3);
+            uglyNumbersSet.add(currentUgly * 5);
+        }
+
+        return currentUgly.intValue(); // Return the nth ugly number
+    }
+}
