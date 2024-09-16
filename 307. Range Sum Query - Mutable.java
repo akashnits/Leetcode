67c13e8804bfdcd56e4a4a2106d05e5a3e2edfc0
Akash <akash.nits@outlook.com>
2024-09-16 14:38:00 +0530

    Update 307. Range Sum Query - Mutable.java

--- 307. Range Sum Query - Mutable.java
+++ 307. Range Sum Query - Mutable.java
@@ -1,84 +1,70 @@
 class NumArray {
-
-    int[] segTree;
-    int[] input;
+    int[] segmentTree;
+    int[] nums;
     int n;
+
     public NumArray(int[] nums) {
+        this.nums = nums;
         n = nums.length;
-        input = nums;
-        int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
-        //Maximum size of segment tree
-        int max_size = 2 * (int) Math.pow(2, x) - 1;
-        segTree = new int[max_size];
-        
-        construct(0, 0, n-1);
+        segmentTree = new int[4 * n];
+        buildSegmentTree(0, 0, n - 1);
     }
     
-    // traverse segment tree and figure out the range which contains the update index
-    // add diff to it
     public void update(int index, int val) {
-        int diff = val - input[index];
-        input[index] = val;
-        updateHelper(0, n-1, 0, index, diff);
+        updateHelper(0, 0, n - 1, index, val);
     }
     
     public int sumRange(int left, int right) {
-        return sumRangeHelper(0, n-1, left, right, 0);
+        return sumRangeHelper(0, 0, n - 1, left, right);
     }
-    
-    int  sumRangeHelper(int start, int end, int left, int right , int i){
-       
-        //  base condition 
-        
-        // invalid
-        if(start > right || end < left){
+
+    private void buildSegmentTree(int i, int l, int r) {
+        if (l == r) {
+            segmentTree[i] = nums[l];
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(2 * i + 1, l, mid);
+        buildSegmentTree(2 * i + 2, mid + 1, r);
+        segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
+    }
+
+    private int sumRangeHelper(int i, int l, int r, int start, int end) {
+        // No overlap
+        if (start > r || end < l) {
             return 0;
         }
-        
-        if (left <= start && right >= end)
-            return segTree[i];
-        
-        int mid = start + (end - start)/2;
-        
-        return sumRangeHelper(start, mid, left, right, 2*i+1) + 
-            sumRangeHelper(mid+1, end, left, right, 2*i+2);
+
+        // Full overlap
+        if (start <= l && r <= end) {
+            return segmentTree[i];
+        }
+
+        // Partial overlap
+        int mid = l + (r - l) / 2;
+        return sumRangeHelper(2 * i + 1, l, mid, start, end) +
+               sumRangeHelper(2 * i + 2, mid + 1, r, start, end);
     }
-    
-    void updateHelper(int start, int end, int i, int index, int diff){
-        if(index < start || index > end){
-            // not in range
+
+    private void updateHelper(int i, int l, int r, int idx, int val) {
+        if (l == r) {
+            // Leaf node - value is stored here
+            segmentTree[i] = val;
             return;
         }
+
+        int mid = l + (r - l) / 2;
         
-        segTree[i] += diff;
-        
-        if( start == end ){
-            return;
+        if (idx <= mid) {
+            // Update left child
+            updateHelper(2 * i + 1, l, mid, idx, val);
+        } else {
+            // Update right child
+            updateHelper(2 * i + 2, mid + 1, r, idx, val);
         }
-        
-        int mid = start + (end - start)/2;
-        updateHelper(start, mid, 2*i+1, index, diff);
-        updateHelper(mid+1, end, 2*i+2, index, diff);
-        return;
-    }
-    
-    int construct(int i , int start , int end){
-        // base condition
-        if(start == end){
-            segTree[i] = input[start];
-            return segTree[i];
-        }
-        
-        int mid = start + (end- start)/2;
-        
-        segTree[i] = construct(2*i+1, start, mid) + construct(2*i+2, mid+1, end);
-        return segTree[i];
+
+        // Update current node
+        segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
     }
 }
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * obj.update(index,val);
- * int param_2 = obj.sumRange(left,right);
- */
