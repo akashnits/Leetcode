370bc109cddaaa3f73e3a5828329eeb14eee08d1
Akash <akash.nits@outlook.com>
2024-10-15 22:58:49 +0530

    Update 731. My Calendar II.java

--- 731. My Calendar II.java
+++ 731. My Calendar II.java
@@ -1,41 +1,44 @@
 class MyCalendarTwo {
-    List<int[]> events = new ArrayList();   // original events
-    TreeMap<Integer, Integer> overlaps = new TreeMap(); // 2-booking overlap intervals
-    // Empty constructor
-    public MyCalendarTwo() {}
+    // Approach: use line sweep algorithm
+    // use map instead of arr to keep track of events
+    // when events come add +1 , substract when they leave -1
+    // prefixSum would give the number of events at that point in time
+    // check if prefixSum <= allowedOverBooking
+
+
+    // stores mapping from time -> +1 or -1 depending on event is entering or leaving
+    TreeMap<Integer, Integer> calendarMap;
+    int allowedBooking = 2;
+    public MyCalendarTwo() {
+        calendarMap = new TreeMap();
+    }
     
-    // Book
     public boolean book(int start, int end) {
-        
-        // check 3-booking
-        if(doesOverlap(overlaps, start, end)){
-            return false;
-        }
-        
-        // no 3-booking
-        // 1. update overlaps
-        for (int[] event: events) {
-            if (event[0] < end && event[1] > start) {     // has an overlap, add the new 2-booking overlaps
-                overlaps.put(Math.max(event[0], start), Math.min(event[1], end));                
+        // step 1
+        calendarMap.put(start, calendarMap.getOrDefault(start, 0) + 1);
+        calendarMap.put(end, calendarMap.getOrDefault(end, 0) - 1);
+
+        // loop over all entries in the map 
+        // calculate prefixSum 
+        // if prefixSum at any point > allowedBooking, this booking isn't allowed
+        // else it's allowed
+
+        int overlappedBookingCount = 0;
+        for(Map.Entry<Integer, Integer> interval: calendarMap.entrySet()){
+            overlappedBookingCount += interval.getValue();
+            if(overlappedBookingCount > allowedBooking){
+                // undo step 1
+                calendarMap.put(start, calendarMap.getOrDefault(start, 0) - 1);
+                calendarMap.put(end, calendarMap.getOrDefault(end, 0) + 1);
+                return false;
             }
         }
-        
-        // 2. update events
-        events.add(new int[]{start, end});
         return true;
     }
-    
-    
-    boolean doesOverlap(TreeMap<Integer, Integer> map, int start, int end){
-        // find if interval overlap with single book
-        Integer prevStartTime = map.floorKey(start);
-        int prevEndTime;
-        if(prevStartTime != null && 
-           ((prevEndTime = map.get(prevStartTime)) > start)) {
-            return true;
-        }
-        Integer nextStartTime = map.ceilingKey(start);
-        if(nextStartTime != null && end > nextStartTime ) return true;
-        return false;
-    }
 }
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
