8269f81276b1884a2d863ebaaec1261b7d4edf6d
Akash <akash.nits@outlook.com>
2025-03-17 22:23:19 +0530

    Update 1026. Maximum Difference Between Node and Ancestor.java

--- 1026. Maximum Difference Between Node and Ancestor.java
+++ 1026. Maximum Difference Between Node and Ancestor.java
@@ -35,3 +35,58 @@ class Solution {
 
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // traverse in post order fashion
+    // find min/max for each subtree and compare with root to find maxDiff
+    // compare it with root value and find maxDiff
+    int maxDiff = 0;
+    public int maxAncestorDiff(TreeNode root) {
+        findMinAndMaxInDecendants(root);
+        return maxDiff;
+    }
+
+    int[] findMinAndMaxInDecendants(TreeNode root){
+        if(root == null){
+            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
+        }
+
+        // post order
+        int[] leftSubTreeMinMax = findMinAndMaxInDecendants(root.left);
+        int[] rightSubTreeMinMax = findMinAndMaxInDecendants(root.right);
+
+        // compare with root val to find maxDiff
+        int currMin = Math.min(leftSubTreeMinMax[0], rightSubTreeMinMax[0]);
+        int currMax = Math.max(leftSubTreeMinMax[1], rightSubTreeMinMax[1]);
+
+        
+        if(currMin != Integer.MAX_VALUE){
+            maxDiff = Math.max(maxDiff, Math.abs(root.val - currMin));
+        } // else this node has no children
+
+        if(currMax != Integer.MIN_VALUE){
+            maxDiff = Math.max(maxDiff, Math.abs(root.val - currMax));
+        }
+
+
+        int newMin = Math.min(root.val, currMin);
+        int newMax = Math.max(root.val, currMax);
+
+        return new int[]{newMin, newMax};
+    }
+}
