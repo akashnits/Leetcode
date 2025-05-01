afdae6eb164913db2b2c2d9696bb080d7c2690b4
Akash <akash.nits@outlook.com>
2025-05-01 12:35:21 +0530

    Create 2563. Count the Number of Fair Pairs.java

---
+++ 2563. Count the Number of Fair Pairs.java
@@ -0,0 +1,57 @@
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        // create a pair array
+        int n = nums.length;
+        Arrays.sort(nums);
+        long res = 0;
+        // loop over the array from end to start
+        for(int i=n-1; i > 0; i--){
+            // try finding pair in the range 0..i-1
+            int lowerVal = lower - nums[i];
+            int upperVal = upper - nums[i];
+
+            // find next greater for lower value; next smaller for upper value
+            int lowerIdx = equalOrNextGreater(nums, 0, i-1, lowerVal);
+            int upperIdx = equalOrNextSmaller(nums, 0, i-1, upperVal);
+
+            // none of the lowerIdx or upperIdx should be invalid
+            if(lowerIdx == -1 || upperIdx == -1){
+                continue;
+            }else{
+                res += upperIdx - lowerIdx + 1;
+            }
+        }
+        return res;
+    }
+
+    int equalOrNextGreater(int[] nums, int l, int r, int val){
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] >= val){
+                // move left
+                r = mid-1;
+            }else{
+                // move right
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+
+    int equalOrNextSmaller(int[] nums, int l, int r, int val){
+        int possibleIdx = -1;
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(nums[mid] <= val){
+                // move right
+                possibleIdx = mid;
+                l = mid+1;
+            }else{
+                // move left
+                r = mid-1;
+            }
+        }
+        return r;
+    }
+
+}
