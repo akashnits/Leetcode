2d47cf57ac2d7f766efd696f59a0a34b4a3a7347
Akash <akash.nits@outlook.com>
2025-06-04 11:24:15 +0530

    Update 92. Reverse Linked List II.java

--- 92. Reverse Linked List II.java
+++ 92. Reverse Linked List II.java
@@ -1,49 +1,41 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        
-        ListNode dummy = new ListNode();
-        dummy.next = head;
-        
-        
-        ListNode l = dummy;
-        while( --left > 0 ){
-            l = l.next;
+        ListNode dummyNode = new ListNode(0);
+        dummyNode.next = head;
+
+        ListNode lPtr = dummyNode;
+        while(--left > 0){
+            lPtr = lPtr.next;
         }
-        
-        ListNode r = head;
-        while( --right > 0){
-            r= r.next;
+
+        ListNode rPtr = dummyNode;
+         while(right-- > 0){
+            rPtr = rPtr.next;
         }
-        
-        ListNode join= r.next;
-        r.next = null;
-        
-        l.next = reverse(l.next, join);
-        return dummy.next;
-            
+
+        ListNode sublistHead = lPtr.next; // original start of sublist
+        ListNode rightList = rPtr.next;   // node after sublist
+
+        // null-check
+        if(sublistHead == null || sublistHead.next == null){
+            return head;
+        }
+
+        ListNode reversedHead = reverseList(sublistHead, sublistHead.next, rightList);
+        sublistHead.next = rightList;     // connect tail of reversed part to rest
+        lPtr.next = reversedHead;         // connect previous part to new head
+
+        return dummyNode.next;
     }
-    
-    
-    ListNode reverse(ListNode head, ListNode join){
-        ListNode prev=null, curr= head, next;
-        
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+
+    // reverse from curr up to (but not including) end
+    ListNode reverseList(ListNode curr, ListNode next, ListNode end) {
+        if (next == end) {
+            return curr;
         }
-        head.next = join;
-        return prev;
+        curr.next = null;
+        ListNode reversedHead = reverseList(next, next.next, end);
+        next.next = curr;
+        return reversedHead;
     }
 }
