5aa247d44cfa3937de304f569e1379e449513c00
Akash <akash.nits@outlook.com>
2026-08-18 16:03:22 +0530

    Refactor stoneGameII method and helper functions

--- 1140. Stone Game II.java
+++ 1140. Stone Game II.java
@@ -1,44 +1,40 @@
 class Solution {
     public int stoneGameII(int[] piles) {
         int n = piles.length;
-        int m = (2 * n);
-        Integer[][][] dp = new Integer[n][m][2];
-        return calculateMaxScoreForAlice(piles, piles.length, 0, 1, 0, dp);
+        Integer[][][] dp = new Integer[n][n+1][2];
+        int res = computeAliceScore(piles, n, 0, 1, 0, dp);
+        return res;
     }
 
-    int calculateMaxScoreForAlice(int[] piles, int n, int i, int m, int turn, Integer[][][] dp){
-        // base condition
-        if( i == n){
-            return 0;
-        }
 
-        if(dp[i][m][turn] != null){
-            return dp[i][m][turn];
-        }
+    int computeAliceScore(int[] piles, int n, int idx, int m, int turn, Integer[][][] dp){
+        if(idx >= n)
+            return 0; // done here
 
-        // what choices does Alice/bob have ?
-        // Alice/Bob can take first X remaining piles which range from 1 ... 2M
+
+        if(dp[idx][m][turn] != null){
+            return dp[idx][m][turn];
+        }
+        
         if(turn == 0){
-            // Alice's turn, she can take first X remaining piles
-            int score = 0;
-            int maxScore = Integer.MIN_VALUE;
-            for(int x=1; x <= Math.min(2*m, n-i); x++){
-                score += piles[i+x -1];
-                maxScore = Math.max(maxScore, score + calculateMaxScoreForAlice(piles, n, i+x, 
-                                                        Math.max(m, x), 1, dp));    
+            // alice's turn 
+            // maximize alice score: we try all choices from 1...2M and recurse
+            int maxScoreAlice = Integer.MIN_VALUE;
+            int currScore = 0;
+            for(int x=1; x <= 2*m && (idx + x -1) < n; x++){ // choice we have
+                currScore += piles[idx + x -1];
+                maxScoreAlice = Math.max(maxScoreAlice, currScore + computeAliceScore(piles, n, idx+x, Math.max(m , x), 1, dp)); 
             }
-            return dp[i][m][turn] = maxScore;
+            dp[idx][m][turn] = maxScoreAlice;
         }else{
-            // Bob's turn, he can take first X remaining piles
-            int score = 0;
-            // minimize Alice's score
-            int minScore = Integer.MAX_VALUE;
-            for(int x=1; x <= Math.min(2*m, n-i); x++){
-                score += piles[i+x -1];
-                minScore = Math.min(minScore,  calculateMaxScoreForAlice(piles, n, i+x, 
-                                                Math.max(m, x), 0, dp));    
+            // bob's turn 
+            // need to minimize alice's score
+            int minScoreAlice = Integer.MAX_VALUE;
+            for(int x=1; x <= 2*m && (idx + x -1) < n; x++){ // choice we have
+                minScoreAlice = Math.min(minScoreAlice, computeAliceScore(piles, n, idx+x, Math.max(m , x), 0, dp)); 
             }
-            return dp[i][m][turn] = minScore;
+            dp[idx][m][turn] = minScoreAlice;
         }
+        return dp[idx][m][turn];
     }
 }
