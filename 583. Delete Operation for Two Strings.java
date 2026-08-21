5592411c4602b5bd9894834e94ef44f4cb8ff9d7
Akash <akash.nits@outlook.com>
2026-08-21 23:47:28 +0530

    Update 583. Delete Operation for Two Strings.java

--- 583. Delete Operation for Two Strings.java
+++ 583. Delete Operation for Two Strings.java
@@ -29,3 +29,41 @@ class Solution {
             return (n+m - 2* dp[n][m]);
     }
 }
+
+
+class Solution {
+    
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        Integer[][] dp = new Integer[n][m];
+
+        return solve(word1, word2, word1.length(), word2.length(), 0, 0, dp);
+    }
+
+    int solve(String word1, String word2, int n, int m, int i, int j, Integer[][] dp) {
+        // base condition:
+        if (i == n)
+            return m - j; // all remaining char in word2
+
+        if (j == m)
+            return n - i; // all remaining char in word1
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        if (word1.charAt(i) == word2.charAt(j)) {
+            // both characters match, no deletion required
+            return dp[i][j] = solve(word1, word2, n, m, i + 1, j + 1, dp);
+        }
+
+        // characters don't match:
+        // either delete word1[i] or delete word2[j]
+        return dp[i][j] = 1 + Math.min(
+            solve(word1, word2, n, m, i + 1, j, dp),
+            solve(word1, word2, n, m, i, j + 1, dp)
+        );
+    }
+}
