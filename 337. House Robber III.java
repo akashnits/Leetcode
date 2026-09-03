dcc22cd651b793055c0088a0756e357a63f0066f
Akash <akash.nits@outlook.com>
2026-09-03 12:39:30 +0530

    Update 337. House Robber III.java

--- 337. House Robber III.java
+++ 337. House Robber III.java
@@ -1,53 +1,40 @@
 class Solution {
     public int rob(TreeNode root) {
-        Map<TreeNodeWithBoolean, Integer> dp = new HashMap();
+        Map<TreeNode, Map<Boolean, Integer>> dp = new HashMap<>();
         return maximumProfit(root, true, dp);
     }
 
+    int maximumProfit(TreeNode root, boolean canRob,
+                      Map<TreeNode, Map<Boolean, Integer>> dp) {
 
-    int maximumProfit(TreeNode root, boolean canRob, Map<TreeNodeWithBoolean, Integer> dp){
+        if (root == null) return 0;
 
-        if(root == null) return 0;
-
-        TreeNodeWithBoolean key = new TreeNodeWithBoolean(root, canRob);
-        if(dp.containsKey(key)){
-            return dp.get(key);
+        // memoization
+        if (dp.containsKey(root) && dp.get(root).containsKey(canRob)) {
+            return dp.get(root).get(canRob);
         }
 
-        if(canRob){
-            // it may or may not rob
-            dp.put(key, Math.max((maximumProfit(root.left, true, dp) + maximumProfit(root.right, true, dp)),
-                           (root.val + maximumProfit(root.left, false, dp) + maximumProfit(root.right, false, dp))));
-            return dp.get(key);               
-        }else{
-            // it must not rob
-            dp.put(key, maximumProfit(root.left, true, dp) + maximumProfit(root.right, true, dp));
-            return dp.get(key);
+        int profit;
+
+        if (canRob) {
+            profit = Math.max(
+                maximumProfit(root.left, true, dp) +
+                maximumProfit(root.right, true, dp),
+
+                root.val +
+                maximumProfit(root.left, false, dp) +
+                maximumProfit(root.right, false, dp)
+            );
+        } else {
+            profit =
+                maximumProfit(root.left, true, dp) +
+                maximumProfit(root.right, true, dp);
         }
 
+        // memoization
+        dp.putIfAbsent(root, new HashMap<>());
+        dp.get(root).put(canRob, profit);
+
+        return profit;
     }
-
-    public class TreeNodeWithBoolean {
-        public TreeNode treeNode;
-        public boolean canRob;
-
-        public TreeNodeWithBoolean(TreeNode treeNode, boolean canRob) {
-            this.treeNode = treeNode;
-            this.canRob = canRob;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            TreeNodeWithBoolean that = (TreeNodeWithBoolean) o;
-            return canRob == that.canRob && Objects.equals(treeNode, that.treeNode);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(treeNode, canRob);
-        }
-    }
-
 }
